c08f49d94d0bf41669602fb07eee780fcfc34391
Fedor Kapranov <fedor.kapranov06@gmail.com>
2026-03-20 00:46:03 +0300

    fetch stocks command

---
+++ application/app/Console/Commands/FetchStocks.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Stock;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class FetchStocks extends WbApiAbstractCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = "fetch:stocks";
+ 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Fetches stock data from the WB API for the current day";
+
+    private const STOCK_VALIDATION_RULES = [
+        'date' => 'nullable|date',
+        'last_change_date' => 'nullable|date',
+        'supplier_article' => 'nullable|string',
+        'tech_size' => 'nullable|string',
+        'barcode' => 'required|integer',
+        'quantity' => 'nullable|integer',
+        'quantity_full' => 'nullable|integer',
+        'is_supply' => 'nullable|boolean',
+        'is_realization' => 'nullable|boolean',
+        'warehouse_name' => 'required|string',
+        'in_way_to_client' => 'nullable|integer',
+        'in_way_from_client' => 'nullable|integer',
+        'nm_id' => 'required|integer',
+        'subject' => 'nullable|string',
+        'category' => 'nullable|string',
+        'brand' => 'nullable|string',
+        'sc_code' => 'required|integer',
+        'price' => 'nullable|numeric',
+        'discount' => 'nullable|numeric',
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Fetches stock data from the WB API for the current day and upserts it into the database.
+     *
+     * The method paginates through all available stock data for today,
+     * validates each record against the defined rules, logs invalid entries,
+     * and inserts or updates valid records using the upsert method.
+     *
+     * @return int Exit code (0 for success)
+     */
+    public function handle(): int
+    {
+        $page = 1;
+
+        do {
+            // Get stocks
+            Log::info("Fetching page=$page");
+
+            $stocks = $this->fetchData(
+                "/stocks",
+                [
+                    "page" => $page,
+                    "dateFrom" => Carbon::today()->format("Y-m-d")
+                ]
+            );
+
+            Log::info("Total amount of fetched stocks: " . \count($stocks));
+
+            $createdRecords = [];
+
+            // Validation
+            foreach ($stocks as $stock) {
+                $validation = Validator::make($stock, self::STOCK_VALIDATION_RULES);
+
+                if ($validation->fails()) {
+                    Log::warning(
+                        "Invalid stock format",
+                        [
+                            "stock" => $stock,
+                            "page" => $page,
+                            "errors" => $validation->failed()
+                        ]
+                    );
+                    continue;
+                }
+                $createdRecords[] = $stock;
+            }
+
+            $affected = Stock::upsert(
+                $createdRecords,
+                ["nm_id", "sc_code", "barcode", "warehouse_name"],
+                [
+                    'date',
+                    'last_change_date',
+                    'supplier_article',
+                    'tech_size',
+                    'quantity',
+                    'is_supply',
+                    'is_realization',
+                    'quantity_full',
+                    'in_way_to_client',
+                    'in_way_from_client',
+                    'subject',
+                    'category',
+                    'brand',
+                    'price',
+                    'discount'
+                ]
+            );
+
+            Log::info("Page=$page processed. Rows upserted: $affected");
+
+            $page++;
+        } while (!empty($stocks));
+        
+        return 0;
+    }
+}
---
+++ application/database/migrations/2026_03_20_123218_create_stocks_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->date("date")->nullable();
+            $table->date("last_change_date")->nullable();
+
+            $table->string("supplier_article")->nullable();
+            $table->string("tech_size")->nullable();
+            $table->integer("barcode");
+            $table->integer("quantity")->nullable();
+            $table->integer("quantity_full")->nullable();
+
+            $table->boolean("is_supply")->nullable();
+            $table->boolean("is_realization")->nullable();
+
+            $table->string("warehouse_name");
+            
+            $table->integer("in_way_to_client")->nullable();
+            $table->integer("in_way_from_client")->nullable();
+
+            $table->integer("nm_id");
+            $table->string("subject")->nullable();
+            $table->string("category")->nullable();
+            $table->string("brand")->nullable();
+            $table->integer("sc_code");
+            
+            $table->decimal("price")->nullable();
+            $table->decimal("discount")->nullable();
+
+            $table->unique(["nm_id", "sc_code", "barcode", "warehouse_name"]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
