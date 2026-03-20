f53c3e84ac3e9e10f372ed99b1f261ea61b0a0f8
Fedor Kapranov <fedor.kapranov06@gmail.com>
2026-03-21 01:51:17 +0300

    Add $fillable and $casts to Income

--- application/app/Models/Income.php
+++ application/app/Models/Income.php
@@ -9,6 +9,36 @@ class Income extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        "income_id",
+        "number",
+        "date",
+        "last_change_date",
+        "supplier_article",
+        "tech_size",
+        "barcode",
+        "quantity",
+        "total_price",
+        "date_close",
+        "warehouse_name",
+        "nm_id"
+    ];
+
+    protected $casts = [
+        "income_id" => "string",
+        "number" => "string",
+        "date" => "date",
+        "last_change_date" => "date",
+        "supplier_article" => "string",
+        "tech_size" => "string",
+        "barcode" => "integer",
+        "quantity" => "integer",
+        "total_price" => "string",
+        "date_close" => "string",
+        "warehouse_name" => "string",
+        "nm_id" => "string",
+    ];
+
     protected $hidden = [
         'updated_at',
         'created_at',
